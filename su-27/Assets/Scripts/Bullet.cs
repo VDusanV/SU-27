281325ba3c27deb8033c5c5251930cfd6365e22c
VDusanV <VDusanV@users.noreply.github.com>
2021-02-01 14:40:54 +0100

    Add menu

--- su-27/Assets/Scripts/Bullet.cs
+++ su-27/Assets/Scripts/Bullet.cs
@@ -7,13 +7,15 @@ public class Bullet : MonoBehaviour
     public int damage = 2;
     public float speed = 20f;
     public Rigidbody2D rb;
+   
     // Start is called before the first frame update
     void Start()
     {
         rb.velocity = transform.up * speed;
-        
-        
+       
+
     }
+  
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
